637b5f231acf0583d46c6069ee227f2264bae28e
ysf1472 <yusuf.ahmet.nihat@gmail.com>
2022-10-23 12:18:01 -0400

    Initial Commit

---
+++ project8-100days/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project8-100days
+//
+//  Created by Yusuf Ahmet Nihat  Hekimoğlu on 2022-10-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
